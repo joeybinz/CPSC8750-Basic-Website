ca59b470e4461af833d03ec122f5589a48fe77d0
NolanRC <55290051+NolanRC@users.noreply.github.com>
2022-09-02 00:34:15 -0400

    Basic Login page

---
+++ 8750-basic-website/src/pages/LoginPage.js
@@ -0,0 +1,71 @@
+
+function LoginPage() {
+  return (
+    <div style={styles.main}>
+      <div style={styles.primaryContainer}>
+        <p style={styles.heading}>Welcome</p>
+        <div style={styles.fieldContainer}>
+          <p style={styles.loginText}>Username:</p>
+          <input type="text" style={styles.fields}/>
+          <p style={styles.loginText}>Password:</p>
+          <input type="text" style={styles.fields}/>
+          <input type="submit" value="Login" style={styles.buttons}/>
+          <input type="submit" value="Signup" style={styles.buttons}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
+
+const styles = {
+  main: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  primaryContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#b2bac2",
+    borderRadius: 10,
+    marginTop: 250,
+    padding: 25,
+    paddingTop: 10,
+  },
+  heading: {
+    color: "black",
+    fontSize: 20,
+    marin: 0,
+  },
+  fieldContainer: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#95a1ad",
+    padding: 15,
+    borderRadius: 10,
+  },
+  loginText: {
+    padding: 0,
+    margin: 0,
+  },
+  fields: {
+    padding: 10,
+    borderRadius: 5,
+    margin: 0,
+    marginBottom: 5,
+  },
+  buttons: {
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
+  },
+  login: {
+
+  },
+  submit: {
+
+  }
+}
