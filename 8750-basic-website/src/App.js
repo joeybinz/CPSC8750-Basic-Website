4fd955b1b59411af806dee70f057691ed8daecdf
Mayank <45217514+mayankdp@users.noreply.github.com>
2022-09-01 23:57:08 -0400

    Finished signup page with navigation

--- 8750-basic-website/src/App.js
+++ 8750-basic-website/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import SignUp from './Sign_Up'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+        </Routes>
+      </nav>
+    </BrowserRouter>
   );
 }
 
---
+++ 8750-basic-website/src/Sign_Up.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function Sign_Up() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to CPSC 8750 first Assignment.</h1>
+                <p>
+                    Sign up to get Started!
+                </p>
+
+                <label>Username:{" "}
+                    <input type="text" name="username" className='Input'></input>
+                </label>
+
+                <label>Email:{" "}
+                    <input type="text" name="email" className='Input'></input>
+                </label>
+
+                <label>Password:{" "}
+                    <input type="text" name="password" className='Input'></input>
+                </label>
+                <button className='Button' onClick={console.log("Home Page")}>Submit</button>
+            </header>
+        </div>
+    );
+}
+
+export default Sign_Up;
